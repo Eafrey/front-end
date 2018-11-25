416e6a4e00b616e4463f56a140a322b95bcd3461
Sen Chen <senchen@thoughtworks.com>
2018-11-25 14:15:37 +0800

    test api with back-end

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,41 @@
 <template>
   <div id="app">
+    <p>ddddd</p>
+    <noscript></noscript>
+    <button @click=testEafreyCN>testApi</button>
+    <p>{{ testContent }}</p>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  data: function() {
+    return { testContent:  'nullll' };
+  },
+  methods: {
+    testEafreyCN() {
+      console.log('clicking')
+      axios.get("/api/test").then(({ data }) => {
+        console.log('testing test api')
+        this.testContent = data;
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
