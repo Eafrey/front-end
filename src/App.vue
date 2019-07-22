5168600a4d536a04f66f616e5338e553a4703adb
Sen Chen <senchen@thoughtworks.com>
2019-07-22 23:43:23 +0800

    add background imgage and improve ui;

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <noscript></noscript>
-    <el-menu background-color="#00BFFF" mode="horizontal" router=true text-color='#FFF'>
+    <el-menu background-color="#00BFFF95" mode="horizontal" router=true text-color='#FFF'>
       <el-menu-item index="/">Home</el-menu-item>
     </el-menu>
     <router-view></router-view> 
@@ -19,13 +19,32 @@ export default {
 </script>
 
 <style>
+html {
+  height: 100%;
+}
+
+body {
+  height: 100%;
+  margin: 0;
+}
+
+.el-menu.el-menu--horizontal {
+    border-bottom: none !important;
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
+  opacity: 0.9;
+  background-image: url(./assets/background.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
 }
+
 p {
   font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
 }
