188d2a0e54fa63daf88f9420221081fa2290c1b0
Sen Chen <senchen@thoughtworks.com>
2019-07-23 00:10:24 +0800

    use scss;

--- src/App.vue
+++ src/App.vue
@@ -18,34 +18,35 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 html {
   height: 100%;
-}
 
-body {
-  height: 100%;
-  margin: 0;
+  body {
+    height: 100%;
+    margin: 0;
+  }
+
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    height: 100%;
+    opacity: 0.9;
+    background-image: url(./assets/background.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  p {
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  }
+
 }
 
 .el-menu.el-menu--horizontal {
     border-bottom: none !important;
-}
-
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  height: 100%;
-  opacity: 0.9;
-  background-image: url(./assets/background.jpg);
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-p {
-  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
 }
 </style>
