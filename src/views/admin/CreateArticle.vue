e28b7cc349eac17e523e9bf0253d9fc7b6f6ea57
Sen Chen <senchen@thoughtworks.com>
2019-07-23 16:46:37 +0800

    add CreateArticle file

---
+++ src/views/admin/CreateArticle.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="container">
+    <editor v-model="editContent" previewStyle="vertical"/>
+    <viewer/>
+</div>
+</template>
+
+<script>
+import 'tui-editor/dist/tui-editor.css';
+import 'tui-editor/dist/tui-editor-contents.css';
+import 'codemirror/lib/codemirror.css';
+import { Editor } from '@toast-ui/vue-editor'
+import 'tui-editor/dist/tui-editor-contents.css';
+import 'highlight.js/styles/github.css';
+import { Viewer } from '@toast-ui/vue-editor'
+export default {
+    data() {
+        return {
+            editContent: '',
+        }
+    },
+    components: {
+        'editor': Editor,
+        'viewer': Viewer
+    }
+}
+</script>
+
+<style scoped>
+.container {
+    height: 100%;
+}
+</style>
