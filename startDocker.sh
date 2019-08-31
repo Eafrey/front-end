ab8e900e69dda045646ab11971a2f07a479d493f
Sen Chen <senchen@thoughtworks.com>
2019-08-31 14:01:00 +0800

    add dockerfile and docker-compose file

---
+++ startDocker.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+rm app.tar.gz
+
+docker-compose down
+
+UNUSEIMAGES=$(docker images --filter "dangling=true" -q --no-trunc)
+if [ "$UNUSEIMAGES" != "" ]; then
+    docker rmi $UNUSEIMAGES
+fi
+docker-compose up -d
